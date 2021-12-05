7502101d375cbfeb213d07116ed37687b6e770ad
Anthony Hien Vu <anthony.vu@checkbox.ai>
2021-12-05 13:01:08 +1100

    Fetch initial tasks from locally running json-server.

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TaskService } from '../../services/task.service';
 import type { Task } from '../../Task';
 
