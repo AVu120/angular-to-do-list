1381012254364f1ec8c3ca22c9d3c0d2273243cd
Anthony Hien Vu <anthony.vu@checkbox.ai>
2021-12-05 12:11:40 +1100

    Use observable to fetch initial tasks.

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from '../../mock-tasks';
+import { TaskService } from '../../services/task.service';
 import type { Task } from '../../Task';
 
 @Component({
@@ -8,8 +8,11 @@ import type { Task } from '../../Task';
   styleUrls: ['./tasks.component.scss'],
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
-  constructor() {}
+  tasks: Task[] = [];
 
-  ngOnInit(): void {}
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
 }
